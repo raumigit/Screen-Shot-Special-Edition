4e97a2eef48c4c3637e8300408c6c4a4015fb0f6
raumigit <86038378+raumigit@users.noreply.github.com>
2022-07-10 21:40:20 +0900

    initial commit

---
+++ screenshot-Su/screenshot-Su/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace screenshot_Su
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mouse_position = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.Debug_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.DataTimer = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.mouseHook1 = new HongliangSoft.Utilities.Gui.MouseHook();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 569);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Not Data";
+            // 
+            // mouse_position
+            // 
+            this.mouse_position.Enabled = true;
+            this.mouse_position.Interval = 30;
+            this.mouse_position.Tick += new System.EventHandler(this.mouse_position_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "クリック検知";
+            // 
+            // Debug_timer1
+            // 
+            this.Debug_timer1.Enabled = true;
+            this.Debug_timer1.Interval = 1000;
+            this.Debug_timer1.Tick += new System.EventHandler(this.Debug_timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "--";
+            // 
+            // DataTimer
+            // 
+            this.DataTimer.Enabled = true;
+            this.DataTimer.Interval = 1000;
+            this.DataTimer.Tick += new System.EventHandler(this.DataTimer_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 569);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "DataTime";
+            // 
+            // mouseHook1
+            // 
+            this.mouseHook1.MouseHooked += new HongliangSoft.Utilities.Gui.MouseHookedEventHandler(this.mouseHook1_MouseHooked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 590);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Screen Shot Special Edition(沙花又フォントプロジェクト)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer mouse_position;
+        private System.Windows.Forms.Label label3;
+        private HongliangSoft.Utilities.Gui.MouseHook mouseHook1;
+        private System.Windows.Forms.Timer Debug_timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer DataTimer;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ screenshot-Su/screenshot-Su/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screenshot_Su
+{
+    public partial class Form1 : Form
+    {
+        //int ScreenShotX, ScreenShotY;
+        //int RangeWidth, RangeHeight;
+        //改良時に利用予定
+
+        bool CaptureLock=false;
+        
+        DateTime nowTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mouse_position_Tick(object sender, EventArgs e)
+        {
+            label2.Text = string.Format("マウス座標：{0:d},{1:d}", Cursor.Position.X, Cursor.Position.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryCheck();
+        }
+
+        public void DirectoryCheck()
+        {
+            try
+            {
+                if (Directory.Exists(@"Capture") == false)
+                    Directory.CreateDirectory("Capture");
+            }
+            catch (Exception)
+            {
+                throw ;
+            }
+            
+        }
+
+        private void mouseHook1_MouseHooked(object sender, HongliangSoft.Utilities.Gui.MouseHookedEventArgs e)
+        {
+            if (e.Message==HongliangSoft.Utilities.Gui.MouseMessage.LDown && CaptureLock==false)
+            {
+                label3.ForeColor = Color.Cyan;
+                ScreenCapture();
+            }
+            if (e.Message == HongliangSoft.Utilities.Gui.MouseMessage.RDown)
+            {
+                label3.ForeColor = Color.Magenta;
+                switch (CaptureLock)
+                {
+                    case true:
+                        CaptureLock = false;
+                        label4.Text = "--";
+                        break;
+
+                    case false:
+                        CaptureLock = true;
+                        label4.Text = "スクリーンショットがロック中";
+                        break;
+
+
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private void DataTimer_Tick(object sender, EventArgs e)
+        {
+            nowTime = DateTime.Now;
+            label5.Text = nowTime.ToString("yyyy-MM-dd|HH:mm:ss");
+        }
+
+        private void Debug_timer1_Tick(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.Black;
+            //Debug_timer1.Enabled = false;
+        }
+
+        public void ScreenCapture()
+        {
+            string saveimage;
+            try
+            {
+                //Bitmap bmp = new Bitmap(128,128);
+                int left, top, width, hight;
+                left = Cursor.Position.X;
+                top = Cursor.Position.Y;
+                width = 128;
+                hight = 128;
+
+
+                /*ここらへんに範囲でスクリーンショットを取る部分を実装予定*/
+                Rectangle rect = new Rectangle(left, top, width, hight);
+                using (var bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb))
+                {
+                    using (var g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size, CopyPixelOperation.SourceCopy);
+                    }
+                    saveimage = nowTime.ToString("yyyy-MM-dd-HH-mm-ss");
+                    //PNG形式で保存する
+                    bmp.Save(string.Format(@"Capture/Capture{0}.png", saveimage, ImageFormat.Png));
+                    bmp.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+    }
+
+
+
+}
